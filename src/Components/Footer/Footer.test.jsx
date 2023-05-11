561e0ec02da2695f5d00b306225e4a3128f14188
Jonathan Tsai <modopo@gmail.com>
2023-05-10 21:56:19 -0700

    useEffect implemented with test

--- src/Components/Footer/Footer.test.jsx
+++ src/Components/Footer/Footer.test.jsx
@@ -2,8 +2,8 @@ import Footer from '.';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-describe('Testing the Header component', () => {
-  test('Shoulder render a h1', () => {
+describe('Testing the footer component', () => {
+  test('Shoulder render footer with copyright symbol', () => {
     render(<Footer />);
 
     const copyright = screen.getByText(/©/i);
